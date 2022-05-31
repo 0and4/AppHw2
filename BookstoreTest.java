d703576033c78a7a50eaa61acfe7e28f7706fa9a
0and4 <iwonchu215@gmail.com>
2022-05-31 23:18:34 +0900

    3. 서점

--- BookstoreTest.java
+++ BookstoreTest.java
@@ -1,8 +1,8 @@
 package apphw2;
 import java.util.Scanner;
 abstract class Book{
-	String name="책이름";
-	int price=0;
+	String name;
+	int price;
 	Book(String name,int price){
 		this.name=name;
 		this.price=price;
@@ -11,110 +11,73 @@ abstract class Book{
 	abstract double salePrice();
 }
 class Comic extends Book{
-	double sale,result;
 	Comic(String name, int price){
-		super("",0);
-		this.name=name;
-		this.price=price;
+		super(name,price);
 		}
-	public void printInfo() { //어떤 용도로 쓰는 메소드인지
-		String gName = "만화책";
+	public void printInfo() {
+		System.out.println("책의 이름은 "+name+"이고 장르는 만화책입니다.");
 	}
 	public double salePrice() {
-		this.sale=0.9;
-		return sale;
-	}
-	double priceSale() {
-		result=price*salePrice();
+		double result=price*0.9;
 		return result;
 	}
 }
 class Fiction extends Book{
-	double sale,result;
 	Fiction(String name, int price){
-		super("",0);
-		this.name=name;
-		this.price=price;
+		super(name,price);
 		}
-	public void printInfo() { //어떤 용도로 쓰는 메소드인지
-		String gName = "소설";
+	public void printInfo() {
+		System.out.println("책의 이름은 "+name+"이고 장르는 소설입니다.");
 	}
 	public double salePrice() {
-		this.sale=0.85;
-		return sale;
-	}
-	double priceSale() {
-		result=price*salePrice();
+		double result=price*0.85;
 		return result;
 	}
 }
 class Magazine extends Book{
-	double sale,result;
 	Magazine(String name, int price){
-		super("",0);
-		this.name=name;
-		this.price=price;
+		super(name,price);
 		}
-	public void printInfo() { //어떤 용도로 쓰는 메소드인지
-		String gName = "잡지";
+	public void printInfo() {
+		System.out.println("책의 이름은 "+name+"이고 장르는 잡지입니다.");
 	}
 	public double salePrice() {
-		this.sale=0.95;
-		return sale;
-	}
-	double priceSale() {
-		result=price*salePrice();
+		double result=price*0.95;
 		return result;
 	}
 }
 class BookStore{
 	Book[] books;
-	String name,gName;
-	double dPrice;
 	double[] dP;
-	int price,genre;
-	String[] gN;
 	BookStore(int num){
 		books = new Book[num];
-		for(int i=0;i<num;i++) {
-			books[i]=new Comic(name,price); //books[i] 초기화하는 방법?
-			dP=new double[num];
-			gN=new String[num];
-		}
+		dP=new double[num];
 	}
-	void setBooks() { 
+	void setBooks() {
 		Scanner sc = new Scanner(System.in);
 		for(int i=0;i<books.length;i++) {
 			System.out.print("책이름: ");
-			name=sc.next();
+			String name=sc.next();
 			System.out.print("책 가격: ");
-			price = sc.nextInt();
+			int price = sc.nextInt();
 			System.out.println("책 장르(1: 코믹, 2: 소설, 3:잡지): ");
-			genre=sc.nextInt();
+			int genre=sc.nextInt();
 			if (genre==1) {
-				Comic c = new Comic(name,price);
-				dPrice=c.priceSale();
-				gName="만화책";
+				books[i]=new Comic(name,price);
 			}
 			else if (genre==2) {
-				Fiction f = new Fiction(name,price);
-				dPrice=f.priceSale();
-				gName="소설";
+				books[i] = new Fiction(name,price);
 			}
 			else if (genre==3) {
-				Magazine m = new Magazine(name,price);
-				dPrice=m.priceSale();
-				gName="잡지";
+				books[i] = new Magazine(name,price);
 			}
-			books[i].name=name;
-			books[i].price=price;
+			double dPrice=books[i].salePrice();
 			dP[i]=dPrice;
-			gN[i]=gName;
 		}
 	}
 	void printBooksInfo() {
 		for(int i=0;i<books.length;i++) {
-			System.out.println("책의 이름은 "+books[i].name+"이고 장르는 "+gN[i]+"입니다.");
+			books[i].printInfo();
 			System.out.println("판매가격은 "+dP[i]+"입니다.");
 		}
 	}
